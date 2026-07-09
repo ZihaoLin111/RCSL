64917d70fa0207c468dea4ec384c883dfda0e5f6
Zihao Lin <216423917+ZihaoLin111@users.noreply.github.com>
2026-07-09 12:48:27 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,16 @@
+set -eu
+
 cd "$(dirname "$0")"
 
 init_txt=glove
 bs=256
 gpu=0
+python_cmd=${PYTHON:-python3}
 data_path=./data
 vocab_path=./vocab
 glove_cache_path=./vocab/vector_cache
+mine_epoch=25
+memory_update_interval=5
 
 tau=0.03
 
@@ -18,8 +23,9 @@ tag=${init_txt}_pl${paired_length}_bs${bs}_tau${tau}
 logger_path=./runsx/${data}_${tag}/log
 model_path=./runsx/${data}_${tag}/checkpoint
 
-CUDA_VISIBLE_DEVICES=$gpu python3 train.py --gpu $gpu  --paired_length $paired_length\
-  --data_name ${data_name} --logger_path $logger_path --model_path $model_path   --init_txt $init_txt \
-  --log_step 200 --embed_size 1024 --tau $tau --batch_size $bs --data_path $data_path \
-  --vocab_path $vocab_path --glove_cache_path $glove_cache_path
+CUDA_VISIBLE_DEVICES=$gpu "$python_cmd" train.py --gpu "$gpu" --paired_length "$paired_length" \
+  --data_name "$data_name" --logger_path "$logger_path" --model_path "$model_path" --init_txt "$init_txt" \
+  --log_step 200 --embed_size 1024 --tau "$tau" --batch_size "$bs" --data_path "$data_path" \
+  --vocab_path "$vocab_path" --glove_cache_path "$glove_cache_path" --MineEpoch "$mine_epoch" \
+  --memory_update_interval "$memory_update_interval"
 done
